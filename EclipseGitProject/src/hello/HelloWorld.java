37b6690c34f15fe319790307ffd129b0bcc5f682
deven09 <devendrakumar09@gmail.com>
2018-05-06 15:54:10 +0530

    deleting second row

--- EclipseGitProject/src/hello/HelloWorld.java
+++ EclipseGitProject/src/hello/HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("second line");
 		
 		System.out.println("Line added from different working dirctory");
 	}
