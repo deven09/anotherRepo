88b525cbf5fbafedabd3d323ed2118cc6a044f35
deven09 <devendrakumar09@gmail.com>
2018-05-30 21:46:12 +0530

    removed method

--- EclipseGitProject/src/hello/HelloWorld.java
+++ EclipseGitProject/src/hello/HelloWorld.java
@@ -2,12 +2,5 @@ package hello;
 
 public class HelloWorld {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		
-		System.out.println("Line added from different working dirctory");
-		
-		System.out.println("another line");
-	}
 
 }
