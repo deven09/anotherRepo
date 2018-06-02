1a3a848accd85f59054e5708d916e74a7da0c5ce
deven09 <devendrakumar09@gmail.com>
2018-06-02 14:43:53 +0530

    Repo2 Line removed by repo1

--- EclipseGitProject/src/hello/HelloWorld.java
+++ EclipseGitProject/src/hello/HelloWorld.java
@@ -3,7 +3,6 @@ package hello;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("First Line from repo2");
 		System.out.println("First line from repo1");
 	}
 	
