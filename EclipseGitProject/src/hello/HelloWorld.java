ede49bd3f2224367ff1f9c839deaa6ff0a8096e3
deven09 <devendrakumar09@gmail.com>
2018-06-02 14:51:02 +0530

    resolved changes from repo1

--- EclipseGitProject/src/hello/HelloWorld.java
+++ EclipseGitProject/src/hello/HelloWorld.java
@@ -6,4 +6,8 @@ public class HelloWorld {
 		System.out.println("First line from repo1");
 	}
 	
+	public static void main(String[] args) {
+		args.getClass();
+		System.out.println("First line from repo1");
+	}
 }
